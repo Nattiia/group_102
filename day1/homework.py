d2ce84e7cd6d3ba42de0a70c58ae0e972930d5bc
Nattiia <natia.razmadze@gmail.com>
2022-10-11 13:18:56 +0400

    first commit

---
+++ day1/homework.py
@@ -0,0 +1,8 @@
+name = "natia"
+surname = "razmadze"
+age = "38"
+heigh = "1.70"
+knows_programing = "false"
+
+if knows_programing:
+        print("chemi saxelia {} da chemi gvaria {} chemi asakia {} da chemi simaglea {}".format(name, surname, age, heigh))
