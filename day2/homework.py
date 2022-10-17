8c5ecb2ea1b09bb9b8fa47995f51a55fef0bc261
Nattiia <natia.razmadze@gmail.com>
2022-10-17 17:16:47 +0400

    day2 commit

---
+++ day2/homework.py
@@ -0,0 +1,57 @@
+name = "natia"
+full_name = "natia razmadze"
+
+# my_sentence = """ადამიანს მშობლიურ სახლში გატარებული 
+# ბავშვობის მოგონებებზე უფრო ძვირფასი არაფერი გააჩნია. 
+# ეს ყოველთვის ასეა, თუკი ოჯახში 
+# მცირედი სიყვარული და თანხმობა მაინც სუფევს"""
+
+# print (my_sentence)
+
+name = "natia"
+surname = "razmadze"
+age = "5"
+knows_programming = "False"
+
+# if knows_programming:
+#     print (name + " " + surname + " " + str(age))
+
+
+# print (name.capitalize())
+# print (name[0])
+# print (name[3])
+
+
+# print (full_name[8])
+
+# arr_of_names = full_name.split()
+# new_arr= ""
+# for i in arr_of_names:
+#     new_arr += i.capitalize() + " "
+# print(new_arr [:-1])   
+
+# print(full_name[2:8])
+
+# name2 = "sandro"
+# print(len(name2))
+
+# if "z" in full_name:
+#     print("gvxvdeba")
+
+# if "h" not in full_name:
+#     print("ar gvxvdeba")
+
+# print(full_name[2:])
+# print(full_name [:3])
+# print(full_name [::2])
+# print(full_name [-1])
+# print(full_name[7:2:-1])
+# print(full_name[9:1:-2])
+# print(full_name[11:1:-3])    #1.start  2.finish  3.step
+
+full_name2 = "NATIA"
+full_name3 = "    natia    "
+# print(full_name.upper())
+# print(full_name2.lower())
+# print(full_name3.strip())
+print(full_name3.replace ("t", "77"))
---
+++ day3/homework.py
@@ -0,0 +1,51 @@
+# my_sentence = "aa {2} bb {0} cc {1}" .format ("xx", "yy", "zz")
+# print (my_sentence)
+
+# if 3>5:
+#     print("a")
+# else:
+#     print("n")
+
+
+# my_age = input("how old are you: ")
+# print(my_age)
+
+# print(type(my_age))
+# print("I am " + my_age + " years old ")
+
+# age = 5
+# age += 3  #incrementation
+# print(age)
+
+
+# a = 20
+# b = 30
+# print(a+b)
+
+# a = "20"
+# b = "30"
+# c = "20"
+# print(a+b+c)
+# print(int(a)+int(b)+int(c))
+
+# num1 = input("enter number1: ")
+# num2 = input("enter number2: ")
+# print("their sum is" +" " + str(num1+num2))
+# print("their sum is" +" " + str(int(num1)+int(num2)))
+
+# num1 = int(input("enter number1: "))
+# num2 = int(input("enter number2: "))
+# print("their sum is " + str (num1+num2))
+
+num1 = int(input("enter number1: "))
+num2 = int(input("enter number2: "))
+product = num1*num2
+print("the result is " + str(num1+num2))
+
+ if(product)>100:
+    print("xxx")
+else:
+    print("yyy")
+
+
+Print(full_name.upper())
